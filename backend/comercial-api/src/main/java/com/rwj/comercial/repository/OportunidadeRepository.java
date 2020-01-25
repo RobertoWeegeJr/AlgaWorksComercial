dd93c204218b784544d19b27cb0b776b26791997
Roberto Weege Jr <robertoweegejr@gmail.com>
2020-01-25 16:20:46 -0300

    Aula 03

---
+++ backend/comercial-api/src/main/java/com/rwj/comercial/repository/OportunidadeRepository.java
@@ -0,0 +1,14 @@
+package com.rwj.comercial.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rwj.comercial.model.Oportunidade;
+
+public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long> {
+	
+	Optional<Oportunidade> findByDescricaoAndNomeProspecto(String descricao, String nomeProspecto);
+	
+	
+}
